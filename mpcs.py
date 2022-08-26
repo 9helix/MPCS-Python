94d8a214fd413abce4e71abd7bab84c592d9181f
9helix <grzinicdino@gmail.com>
2022-08-26 21:13:46 +0200

    bug fixes

--- mpcs.py
+++ mpcs.py
@@ -1,4 +1,4 @@
-import pyperclip as pc #from matplotlib.backend_bases import MouseButton
+import pyperclip as pc  # from matplotlib.backend_bases import MouseButton
 from PyQt5 import QtGui
 from urllib import request
 from bs4 import BeautifulSoup as bs
@@ -6,18 +6,19 @@ import validators
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import configparser as cp
+import os
 
-config=cp.ConfigParser()
+config = cp.ConfigParser()
 config.read('settings.ini')
-fov=config['DEFAULT'].getint('FOV')
+fov = config['DEFAULT'].getint('FOV')
 # processing website data
 
 err = True
 
 
 while err:
-    url=input('Enter offset URL: ')
-    #url = "https://cgi.minorplanetcenter.net/cgi-bin/uncertaintymap.cgi?Obj=P11wdHj&JD=2459810.791667&Form=Y&Ext=VAR&OC=000&META=apm00"
+    url = input('Enter offset URL: ')
+    # url = "https://cgi.minorplanetcenter.net/cgi-bin/uncertaintymap.cgi?Obj=P11wdHj&JD=2459810.791667&Form=Y&Ext=VAR&OC=000&META=apm00"
     obj_name = url[url.find('Obj=')+4:url.find('&JD')]
     if not validators.url(url):
         print("Invalid URL\n")
@@ -62,8 +63,6 @@ for line in content:
     points[coords] = color
 
 
-
-
 print('\nPlotting data ...\n')
 
 
@@ -76,7 +75,8 @@ class BlittedCursor:
         self.ax = ax
         self.background = None
 
-        self.rect=patches.Rectangle(width=fov,height=fov,color='c',lw=2,ls='-',xy=(0,0),fill=False)
+        self.rect = patches.Rectangle(
+            width=fov, height=fov, color='c', lw=2, ls='-', xy=(0, 0), fill=False)
         self.ax.add_patch(self.rect)
         self.text = ax.text(0.67, 0.93, '', transform=ax.transAxes)
         self._creating_background = False
@@ -115,9 +115,9 @@ class BlittedCursor:
             self.set_cross_hair_visible(True)
             # update the line positions
             x, y = event.xdata, event.ydata
-            self.rect.set(xy=(x-fov/2,y-fov/2))
-            self.text.set_position((0.69-(0.02*len(str(int(x)))),0.93)) 
-            #print(x,y)
+            self.rect.set(xy=(x-fov/2, y-fov/2))
+            self.text.set_position((0.69-(0.02*len(str(int(x)))), 0.93))
+            # print(x,y)
             self.text.set_text('R.A.=%1.2f", Decl.=%1.2f"' % (x, y))
 
             self.ax.figure.canvas.restore_region(self.background)
@@ -205,61 +205,63 @@ class Click():
         self.press = False
         self.move = False
 
-first=True
+
+first = True
+
+
 def coordclick(event):
     global first
     x, y = map(round, (event.xdata, event.ydata))
 
-    if x<0:
-        x=-x
-        x_hr=-(x//3600)
-        x_min=-((x-x_hr*3600)//60)
-        x_sec=x-x_hr*3600-x_min*60
+    if x < 0:
+        x = -x
+        x_hr = -(x//3600)
+        x_min = -((x-x_hr*3600)//60)
+        x_sec = x-x_hr*3600-x_min*60
         x_min = str(x_min).zfill(2)
         x_sec = str(x_sec).zfill(2)
         x_hr = '-'+str(x_hr).zfill(2)
     else:
-        x_hr=x//3600
-        x_min=(x-x_hr*3600)//60
-        x_sec=x-x_hr*3600-x_min*60
+        x_hr = x//3600
+        x_min = (x-x_hr*3600)//60
+        x_sec = x-x_hr*3600-x_min*60
         x_min = str(x_min).zfill(2)
         x_sec = str(x_sec).zfill(2)
         x_hr = '+'+str(x_hr).zfill(2)
 
-    if y<0:
-        y=-y
-        y_deg=-(y//3600)
-        y_min=-((y-y_deg*3600)//60)
-        y_sec=y-y_deg*3600-y_min*60
+    if y < 0:
+        y = -y
+        y_deg = -(y//3600)
+        y_min = -((y-y_deg*3600)//60)
+        y_sec = y-y_deg*3600-y_min*60
         y_min = str(y_min).zfill(2)
-        y_sec = str(y_sec).zfill(2)        
+        y_sec = str(y_sec).zfill(2)
         y_deg = '-'+str(y_deg).zfill(2)
     else:
-        y_deg=y//3600
-        y_min=(y-y_deg*3600)//60
-        y_sec=y-y_deg*3600-y_min*60
+        y_deg = y//3600
+        y_min = (y-y_deg*3600)//60
+        y_sec = y-y_deg*3600-y_min*60
         y_min = str(y_min).zfill(2)
         y_sec = str(y_sec).zfill(2)
         y_deg = '+'+str(y_deg).zfill(2)
-    output=f'{x_hr}h {x_min}m {x_sec}s , {y_deg}° {y_min}\' {y_sec}"'
+    output = f'{x_hr}h {x_min}m {x_sec}s , {y_deg}° {y_min}\' {y_sec}"'
 
     print(output)
     pc.copy(output)
     print('Copied to clipboard.\n')
     if first:
-        f=open('coords.txt','w')
+        f = open('coords.txt', 'w')
         f.write(output+'\n')
         f.close()
-        first=False
+        first = False
     else:
-        f=open('coords.txt','a')
+        f = open('coords.txt', 'a')
         f.write(output+'\n')
         f.close()
-        
 
 
 thismanager.window.setWindowIcon(QtGui.QIcon(
-    r"D:\Preuzimanja\Icons\ICO\meteorite.ico"))  # os.path.dirname(__file__) + r"\ICO\meteorite.ico"
+    rf"{os.path.dirname(__file__)}\meteor.ico"))
 plt.gca().invert_xaxis()
 plt.ticklabel_format(style='plain')
 plt.tight_layout()
